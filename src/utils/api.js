14644db03ac54dc8856c5f892721c0b5fa5a9316
tech-ge <geoffreymaitha044@gmail.com>
2025-12-22 20:15:36 +0300

    Change API base URL to production endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5002/api';
+const API_BASE_URL = ' wss://your-project.up.railway.app';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
