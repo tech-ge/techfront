f8f4e73f76733ba6cc72c9527e49088838b14025
tech-ge <geoffreymaitha044@gmail.com>
2025-12-23 10:34:49 +0300

    Change socket connection URL to production

--- src/pages/ChatPageNew.js
+++ src/pages/ChatPageNew.js
@@ -113,7 +113,7 @@ const ChatPageNew = () => {
   useEffect(() => {
     if (!user) return;
 
-    const newSocket = io('http://localhost:5002', {
+    const newSocket = io('https://your-project.up.railway.app', {
       reconnection: true,
       reconnectionDelay: 1000,
       reconnectionAttempts: 5
